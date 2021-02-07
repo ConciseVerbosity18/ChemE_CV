962a10e292d521254811d8e2319dfe406ac94eac
ConciseVerbosity18 <77504170+ConciseVerbosity18@users.noreply.github.com>
2021-02-07 02:04:00 -0700

    Update setup.py
    Changed the source to be correct

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ChemE',
     version='0.0.3',
     description='Various functions and data for Chemical Engineering Students',
-    url='https://github.com/ConciseVerbosity18/ChemE.git',
+    url='https://github.com/ConciseVerbosity18/ChemE_CV.git',
     author='ol-<(',
     include_package_data=True,
     package_data={'':['Data_files/*.txt']},
